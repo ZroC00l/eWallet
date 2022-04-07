3dde6c8ebdb7ed2203e209057f05a4fa00906e8a
ZroC00l <giftgabanakgosi@gmail.com>
2022-04-07 17:57:35 +0200

    Payment methods section

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -19,8 +19,22 @@ const ScanScreen = ({navigation}) => {
           onPress={() => navigation.navigate('HomeScreen')}>
           <Image source={icons.close} style={styles.imageView} />
         </TouchableOpacity>
+
+        <View style={styles.view4}>
+          <Text style={styles.text}>Scan for Payment</Text>
+        </View>
+
+        <TouchableOpacity
+          style={styles.view5}
+          onPress={() => console.log('Info')}>
+          <Image source={icons.info} style={styles.imageView2} />
+        </TouchableOpacity>
       </View>
     );
+  }
+
+  function renderPaymentMethods() {
+    return <View style={styles.view6}></View>;
   }
 
   return (
@@ -40,6 +54,7 @@ const ScanScreen = ({navigation}) => {
           buttonNegative: 'Cancel',
         }}>
         {renderHeader()}
+        {renderPaymentMethods()}
       </RNCamera>
     </View>
   );
@@ -62,9 +77,41 @@ const styles = StyleSheet.create({
     marginTop: SIZES.padding * 4,
     paddingHorizontal: SIZES.padding * 3,
   },
+  view4: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view5: {
+    height: 45,
+    width: 45,
+    backgroundColor: COLORS.green,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view6: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 220,
+    padding: SIZES.padding * 3,
+    borderTopLeftRadius: SIZES.radius,
+    borderTopRightRadius: SIZES.radius,
+  },
   imageView: {
     height: 20,
     width: 20,
     tintColor: COLORS.white,
+  },
+  imageView2: {
+    height: 25,
+    width: 25,
+    tintColor: COLORS.white,
+  },
+  text: {
+    ...FONTS.body3,
+    color: COLORS.white,
   },
 });
